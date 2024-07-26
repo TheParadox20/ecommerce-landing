0767268785f7d6173eecd1ac1df54dd071f6028e
TheParadox20 <onyambusamson@gmail.com>
2024-07-26 14:34:03 +0300

    setup maintenance page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors:{
+        primary: {
+          base: '#6d31ed',
+          light: '#2C2C2C',
+          dark: '#0C0C0C',
+        },
+        secondary: {
+          base: '#15abff',
+          light: '#F5F5F5',
+          dark: '#D5D5D5',
+        },
+        warning: '#FF0000',
+        success: '#00FF00',
+        danger: '#FF0000',
+        info: '#0000FF',
+        Gray: '#A9A9A9',
+        Grey: '#808080',
+        Black: '#000000',
+        White: '#FFFFFF',
+      }
     },
   },
   plugins: [],
